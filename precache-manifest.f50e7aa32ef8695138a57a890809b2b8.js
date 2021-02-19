55b39677f8cd8669b6173e36417c993a1a25898c
Aya Adel <aya.adel.wd@gmail.com>
2021-02-19 15:41:37 +0200

    Updates

---
+++ precache-manifest.f50e7aa32ef8695138a57a890809b2b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938c3b381eef1323856d5d693c7a60a1",
+    "url": "/gemeni/index.html"
+  },
+  {
+    "revision": "4f3d7e4de6ab592227f6",
+    "url": "/gemeni/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "6fd243b816c3b6a8d46f",
+    "url": "/gemeni/static/css/main.2ebe2dab.chunk.css"
+  },
+  {
+    "revision": "4f3d7e4de6ab592227f6",
+    "url": "/gemeni/static/js/2.94e8a236.chunk.js"
+  },
+  {
+    "revision": "db94e03f5604d0bb4bc267fa4b4b74f0",
+    "url": "/gemeni/static/js/2.94e8a236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd243b816c3b6a8d46f",
+    "url": "/gemeni/static/js/main.003e94e2.chunk.js"
+  },
+  {
+    "revision": "74b3777a790a9f704822",
+    "url": "/gemeni/static/js/runtime-main.b4b3cc9c.js"
+  }
+]);
